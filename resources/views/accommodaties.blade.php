4fb11d6b7f79c7653b539ece3b56cc8d428720a8
yaelscholten <yaelscholten1@gmail.com>
2024-04-28 22:31:21 +0200

    Update accommodaties.blade.php

--- resources/views/accommodaties.blade.php
+++ resources/views/accommodaties.blade.php
@@ -11,16 +11,16 @@
 </div>
 
 <div class="w-full h-auto pb-[5rem]">
-        <div class="max-w-[1400px] h-full p-[1rem] md:p-[2rem] lg:p-[4rem] bg-white rounded-[25px] -mt-[4rem] mx-auto flex flex-wrap justify-center gap-[2rem]">
-                @foreach($accommodaties as $accommodatie)
-                    <a href="" class="w-[100%] md:w-[50%] lg:w-[30%] h-auto border-[1px] border-[#efefef] rounded-[10px]">
-                        <img class="w-full h-[300px] object-cover object-center rounded-tr-[10px] rounded-tl-[10px]" src="{{ Vite::asset('resources/images/' . $accommodatie['afbeelding_url']) }}">
-                        <div class="p-[2rem]">
-                            <h2 class="text-[24px] font-[600] mb-[1rem] text-center">{{ $accommodatie['titel'] }}</h2>
-                        </div>
-                    </a>
-                @endforeach
-        </div>
+    <div class="max-w-[1400px] h-full p-[1rem] md:p-[2rem] lg:p-[4rem] bg-white rounded-[25px] -mt-[4rem] mx-auto flex flex-wrap justify-center gap-[2rem]">
+        @foreach($accommodaties as $accommodatie)
+            <a href="" class="w-[100%] md:w-[50%] lg:w-[30%] h-auto border-[1px] border-[#efefef] rounded-[10px]">
+                <img class="w-full h-[300px] object-cover object-center rounded-tr-[10px] rounded-tl-[10px]" src="{{ Vite::asset('resources/images/' . $accommodatie['afbeelding_url']) }}">
+                <div class="p-[2rem]">
+                    <h2 class="text-[24px] font-[600] mb-[1rem] text-center">{{ $accommodatie['titel'] }}</h2>
+                </div>
+            </a>
+        @endforeach
     </div>
+</div>
 
 @endsection
